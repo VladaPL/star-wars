ef323bf5ebcca154ba38e80aafc12270f6d67cf2
Vlada Plutalova <vladaplutalova@yandex.ru>
2023-01-26 12:28:02 +0300

    feat: add compose func to hoc-helpers and to item-list

--- src/components/sw-components/item-list .js
+++ src/components/sw-components/item-list .js
@@ -1,14 +1,13 @@
 import React from "react";
 import ItemList from "../item-list";
-import { withData, withSwapiService } from "../hoc-helpers";
+import {
+    withData,
+    withSwapiService,
+    withChildFunction,
+    compose,
+} from "../hoc-helpers";
 
 // * Partially applied func - техника частично примененные ф-ии
-
-const withChildFunction = (fn) => (Wrapped) => {
-    return (props) => {
-        return <Wrapped {...props}>{fn}</Wrapped>;
-    };
-};
 
 const renderName = ({ name }) => <span>{name}</span>;
 const renderModelAndName = ({ model, name }) => (
@@ -35,17 +34,23 @@ const mapStarshipMethodsToProps = (swapiService) => {
     };
 };
 
-const PersonList = withSwapiService(mapPersonMethodsToProps)(
-    withData(withChildFunction(renderName)(ItemList))
-);
+const PersonList = compose(
+    withSwapiService(mapPersonMethodsToProps),
+    withData,
+    withChildFunction(renderName)
+)(ItemList);
 
-const PlanetList = withSwapiService(mapPlanetMethodsToProps)(
-    withData(withChildFunction(renderName)(ItemList))
-);
+const PlanetList = compose(
+    withSwapiService(mapPlanetMethodsToProps),
+    withData,
+    withChildFunction(renderName)
+)(ItemList);
 
-const StarshipList = withSwapiService(mapStarshipMethodsToProps)(
-    withData(withChildFunction(renderModelAndName)(ItemList))
-);
+const StarshipList = compose(
+    withSwapiService(mapStarshipMethodsToProps),
+    withData,
+    withChildFunction(renderModelAndName)
+)(ItemList);
 
 // Каждый компонент высшего порядка, после того, как мы ее частично сфигурировали,
 // принимает ровно один аргумент - это тот компонент, который нужно обернуть.
@@ -56,15 +61,15 @@ export { PersonList, PlanetList, StarshipList };
 
 // * Создание ф-ии compose
 
-const compose =
-    (...funcs) =>
-    (comp) => {
-        // ???
-    };
+// const compose =
+//     (...funcs) =>
+//     (comp) => {
+         // ???
+//     };
 // проходимся по массиву аргумента ф-ии компоуз ([f,f,f] из ф-ий состоит)  и передаем каждой ф-ии результат предыдущей ф-ии
 // возвращаем самый последний результат ф-ии компоуз
 
-compose(a, b, c)(value);
+//compose(a, b, c)(value);
 // и
-a(b(c(value)));
+//a(b(c(value)));
 // это одно и то же
